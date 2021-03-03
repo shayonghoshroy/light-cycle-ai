38dd2965d9db17852270f5dc50adca58f3d45a36
Shayon Ghoshroy <shayonghoshroy@Shayons-MacBook-Pro.local>
2021-03-03 01:48:30 -0500

    began implementing neat

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import neat
 import pygame, sys, random
 from pygame import mixer
 from pygame.locals import *
@@ -18,6 +20,7 @@ BLUE_BIKE = pygame.transform.scale(pygame.image.load("imgs/blue_bike.png"), (TIL
 PINK_BIKE = pygame.transform.scale(pygame.image.load("imgs/pink_bike.png"), (TILE_SIZE, TILE_SIZE))
 WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 WIN.set_alpha(None)
+gen = 0
 
 
 class Grid:
@@ -27,11 +30,13 @@ class Grid:
                 pygame.draw.line(WIN, LIGHTGREY, (x, 0), (x, WIN_HEIGHT))
                 pygame.draw.line(WIN, LIGHTGREY, (0, y), (WIN_WIDTH, y))
 
+
     def draw_grid(self, blue_bike, pink_bike):
         for (x, y) in blue_bike.visited:
             pygame.draw.rect(WIN, BLUE, (x, y, TILE_SIZE, TILE_SIZE))
         for (x, y) in pink_bike.visited:
             pygame.draw.rect(WIN, PINK, (x, y, TILE_SIZE, TILE_SIZE))
+
 
 class Bike:
     def __init__(self, x, y, IMG, X_VEL, Y_VEL):
@@ -53,7 +58,7 @@ class Bike:
 
     def move(self):
         self.visited += [(self.x, self.y)]
-        #self.visited += [((self.x*2+self.X_VEL)/2, (self.y*2+self.Y_VEL)/2)]  # fill in the midpoint
+        pygame.draw.rect(WIN, BLUE, (self.x, self.y, TILE_SIZE, TILE_SIZE))
         self.x += self.X_VEL
         self.y += self.Y_VEL
 
@@ -105,11 +110,13 @@ def write(word, color):
 
 def write_scores(blue_score, pink_score):
     font = pygame.font.Font('freesansbold.ttf', 48)
-    text = font.render(blue_score, True, BLUE, BLACK)
+    text = font.render(blue_score, True, BLUE)
+    text.set_alpha(200)
     text_rect = text.get_rect()
     text_rect.center = (WIN_WIDTH - text.get_width(), text.get_height())
     WIN.blit(text, text_rect)
-    text = font.render(pink_score, True, PINK, BLACK)
+    text = font.render(pink_score, True, PINK)
+    text.set_alpha(200)
     text_rect = text.get_rect()
     text_rect.center = (WIN_WIDTH - text.get_width(), text.get_height() * 2)
     WIN.blit(text, text_rect)
@@ -142,7 +149,58 @@ def check_collisions(grid, blue_bike, pink_bike, blue_score, pink_score):
         reset_players(grid, blue_bike, pink_bike, blue_score, pink_score)
     return blue_score, pink_score
 
-# TODO: MAKE A DUPLICATE OF GAME FUNCTION FOR AI VERSION, HAVE PLAYER CHOOSE AGAINST CPU OR 2 PLAYER
+
+def fitness(genomes, config):
+    WIN.fill(BLACK)
+    grid = Grid()
+
+    global gen
+    gen += 1
+    nets = []  # neural network for each bike pair
+    bikes = []  # corresponding bike pair
+    ge = []  # corresponding genomes
+
+    for genome_id, genome in genomes:
+        genome.fitness = 0
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        nets.append(net)
+        # each element is a (blue_bike, pink_bike) pair
+        # EXAMPLES
+        # 1. this prints the bikes in the first pair
+        # print(bikes[0][0])
+        # print(bikes[0][1])
+        # 2. looping through the list
+        # for bike in bikes:
+        #   print(bike[0], bike[1])
+        bikes.append((Bike(0, 0, BLUE_BIKE, BIKE_SPEED, 0),
+                      Bike(WIN_WIDTH, WIN_HEIGHT-TILE_SIZE, PINK_BIKE, -BIKE_SPEED, 0)))
+
+    while True:
+        clock = pygame.time.Clock()
+        clock.tick(120)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        for blue_bike, pink_bike in bikes:
+            blue_bike.move()
+            pink_bike.move()
+            grid.draw_grid(blue_bike, pink_bike)
+            blue_bike.draw()
+            pink_bike.draw()
+            pygame.display.update()
+            if (blue_bike.x, blue_bike.y) in pink_bike.visited or (blue_bike.x, blue_bike.y) in blue_bike.visited \
+                    or blue_bike.x < 0 or blue_bike.x > WIN_WIDTH or blue_bike.y < 0 or blue_bike.y > WIN_HEIGHT:
+                pass
+                # reset_players(grid, blue_bike, pink_bike, blue_score, pink_score)
+            elif (pink_bike.x, pink_bike.y) in blue_bike.visited or (pink_bike.x, pink_bike.y) in pink_bike.visited \
+                    or pink_bike.x < 0 or pink_bike.x > WIN_WIDTH or pink_bike.y < 0 or pink_bike.y > WIN_HEIGHT:
+                pass
+                # reset_players(grid, blue_bike, pink_bike, blue_score, pink_score)
+
+
 
 def game():
     WIN.fill(BLACK)
@@ -157,7 +215,7 @@ def game():
 
     while True:
         clock = pygame.time.Clock()
-        clock.tick(120)
+        clock.tick(60)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -185,5 +243,51 @@ def game():
         blue_score, pink_score = check_collisions(grid, blue_bike, pink_bike, blue_score, pink_score)
 
 
+def run_neat_config(config_path):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                                config_path)
+    p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    p.add(neat.StatisticsReporter())
+
+    winner = p.run(fitness, 50)
+
+
 if __name__ == '__main__':
-    game()
+    # game()
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config-feedforward.txt")
+    run_neat_config(config_path)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
